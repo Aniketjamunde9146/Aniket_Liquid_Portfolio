c6e8271d50e2e30909c32c636ddffd1a9a56ee4e
ANIKET JAMUNDE <aniketjamunde4@gmail.com>
2026-05-25 22:46:29 +0530

    bing bro seo pushed

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,7 +122,7 @@ export default function RootLayout({
 
         {/* ✅ Favicons & PWA */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icon.png" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
